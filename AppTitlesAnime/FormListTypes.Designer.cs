15e7339abe948ed7f21ad8eb4a8c627e3ef52007
Павел Кузеванов <kuzya_pasha_run@mail.ru>
2025-04-08 12:17:42 +0500

    Добавлено отображение справочника Типы аниме

--- AppTitlesAnime/FormListTypes.Designer.cs
+++ AppTitlesAnime/FormListTypes.Designer.cs
@@ -50,7 +50,7 @@
             flowLayoutPanelTop.Location = new Point(0, 0);
             flowLayoutPanelTop.Name = "flowLayoutPanelTop";
             flowLayoutPanelTop.Padding = new Padding(10);
-            flowLayoutPanelTop.Size = new Size(851, 61);
+            flowLayoutPanelTop.Size = new Size(934, 61);
             flowLayoutPanelTop.TabIndex = 0;
             // 
             // btnAddType
@@ -86,13 +86,16 @@
             // 
             // dataGridViewTypes
             // 
+            dataGridViewTypes.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dataGridViewTypes.BackgroundColor = Color.White;
             dataGridViewTypes.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridViewTypes.Dock = DockStyle.Fill;
             dataGridViewTypes.Location = new Point(10, 10);
+            dataGridViewTypes.MultiSelect = false;
             dataGridViewTypes.Name = "dataGridViewTypes";
             dataGridViewTypes.ReadOnly = true;
-            dataGridViewTypes.Size = new Size(831, 376);
+            dataGridViewTypes.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridViewTypes.Size = new Size(914, 430);
             dataGridViewTypes.TabIndex = 1;
             // 
             // panelFill
@@ -102,7 +105,7 @@
             panelFill.Location = new Point(0, 61);
             panelFill.Name = "panelFill";
             panelFill.Padding = new Padding(10);
-            panelFill.Size = new Size(851, 396);
+            panelFill.Size = new Size(934, 450);
             panelFill.TabIndex = 2;
             // 
             // FormListTypes
@@ -110,7 +113,7 @@
             AutoScaleDimensions = new SizeF(11F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(851, 457);
+            ClientSize = new Size(934, 511);
             Controls.Add(panelFill);
             Controls.Add(flowLayoutPanelTop);
             Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
--- AppTitlesAnime/FormListTypes.cs
+++ AppTitlesAnime/FormListTypes.cs
@@ -1,10 +1,40 @@
-﻿namespace AppTitlesAnime
+﻿using System.ComponentModel;
+using AppTitlesAnime.Models;
+using Microsoft.EntityFrameworkCore;
+using AppContext = AppTitlesAnime.Models.AppContext;
+
+namespace AppTitlesAnime
 {
     public partial class FormListTypes : Form
     {
+        private AppContext db;
         public FormListTypes()
         {
             InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            this.db = new AppContext();
+            this.db.Types.Load();
+            this.dataGridViewTypes.DataSource = this.db.Types.Local.OrderBy(o=>o.TypeName).ToList();
+
+            //скрытие столбцов
+            dataGridViewTypes.Columns["Id"].Visible = false;
+            dataGridViewTypes.Columns["AnimeTitles"].Visible = false;
+
+            //изменение названий заголовков столбцов
+            dataGridViewTypes.Columns["TypeName"].HeaderText = "Тип аниме";
+
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+
+            this.db?.Dispose();
+            this.db = null;
         }
 
         private void BtnAddType_Click(object sender, EventArgs e)
