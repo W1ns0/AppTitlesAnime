34e522cfb13a95cd15b60df8bfc15c93a2d01a1d
Павел Кузеванов <kuzya_pasha_run@mail.ru>
2025-04-08 11:00:25 +0500

    Добавлены формы для работы со справочником Типы аниме

---
+++ AppTitlesAnime/FormAddType.cs
@@ -0,0 +1,25 @@
+﻿namespace AppTitlesAnime
+{
+    public partial class FormAddType : Form
+    {
+        public FormAddType()
+        {
+            InitializeComponent();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void FormAddType_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxTypeName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- AppTitlesAnime/Models/AppContext.cs
+++ AppTitlesAnime/Models/AppContext.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace AppTitlesAnime.Models;
 
@@ -28,7 +26,6 @@ public partial class AppContext : DbContext
     public virtual DbSet<Type> Types { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=db_anime_titles;Username=postgres;Password=1111");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
